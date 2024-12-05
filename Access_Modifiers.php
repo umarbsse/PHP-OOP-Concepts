29ea0bb7028d7072c28512db84b351b24a3a7c5f
umarbsse <umarbsse@gmail.com>
2024-12-05 22:16:18 +0500

    Access_Modifiers

--- Access_Modifiers.php
+++ Access_Modifiers.php
@@ -3,10 +3,26 @@
         public $name;
         protected $color;
         private $weight;
+
+        public function set_name($name){
+            $this->name = $name;
+        }
+        protected function set_color($color){
+            $this->color = $color;
+        }
+        private function set_weight($weight){
+            $this->weight = $weight;
+        }
     }
 
     $mango = new Fruit();
-    $mango->name = "Mango";
-    $mango->color  = "Yellow";
-    $mango->weight = "1 KG";
+    $mango->set_name('Mango'); // OK
+    $mango->set_color('Yellow'); //Error
+    //$mango->name = "Mango";// OK
+    //$mango->color  = "Yellow"; //Error
+    //$mango->weight = "1 KG"; //Error
+
+    echo $mango->name;
+    echo "<br>";
+    echo $mango->color;
 ?>
