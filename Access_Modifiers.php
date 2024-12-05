18560520eae1000d1edac4eaee332a48b47062d8
umarbsse <umarbsse@gmail.com>
2024-12-05 22:21:03 +0500

    Access_Modifiers

--- Access_Modifiers.php
+++ Access_Modifiers.php
@@ -18,6 +18,7 @@
     $mango = new Fruit();
     $mango->set_name('Mango'); // OK
     $mango->set_color('Yellow'); //Error
+    $mango->set_weight('1 KG'); //Error
     //$mango->name = "Mango";// OK
     //$mango->color  = "Yellow"; //Error
     //$mango->weight = "1 KG"; //Error
